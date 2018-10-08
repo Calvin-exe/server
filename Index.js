341503e809e49063a8e83a45fd973dcf46524d41
Calvin-exe <wynternerd@gmail.com>
2018-10-08 09:19:22 -0800

    initial commit

---
+++ Index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/',(req,res) => res.send('hello world!'));
+app.get('/about',(req,res) => res.send('hElLO wOrLD!'));
+
+app.listen(port,() => console.log('server works!'));
